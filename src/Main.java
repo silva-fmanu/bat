ff1ae98de8a71ae16dfb1e6d5fa4b2feae3a1900
Manuela Silva <silvafarias.manu07@gmail.com>
2024-04-12 10:19:49 -0300

    teste

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner ler=new Scanner(System.in);
+        String[][] tabuleiro=new String[10][10];
+        mostraTabuleiro(tabuleiro);
+
+        System.out.print("Linha:");
+        int linha=ler.nextInt();
+        System.out.print("Coluna:");
+        String coluna=ler.next().toLowerCase();
+
+        marcarNaMatriz(linha,coluna);
+
+        System.out.println(linha);
+        System.out.println(coluna);
+    }
+    static void marcarNaMatriz(int lin, String col){
+        for (int i=0;i<10;i++){
+
+        }
+    }
+
+    static void mostraTabuleiro(String[][]campo){
+        System.out.println("|||A| B| C| D| E| F| G| H| I| J| K|");
+        for (int i = 0; i < campo.length; i++) {
+            System.out.print(i+"|");
+            for (int j = 0; j < campo[i].length; j++) {
+                System.out.print("♒|");
+            }
+            System.out.println();
+        }
+    }
+}
